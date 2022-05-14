2e9b4a5cb11f1b76be6a1821c6ef67ac5b83babe
seows2 <seows2@naver.com>
2022-05-14 23:05:15 +0900

    WIP: mouse1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { useState } from "react";
+import { currentDay, getHour } from "./utils/date";
+
+type DayTime = "night" | "day";
+
 function App() {
-  return <div className="App">App</div>;
+  const [dayTime, setDayTime] = useState<DayTime>(currentDay(getHour()));
+
+  return <div className="App">{dayTime}</div>;
 }
 
 export default App;
---
+++ src/Components/Mouse/index.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+const Mouse = () => {
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0, opacity: 0 });
+};
+
+export default Mouse;
