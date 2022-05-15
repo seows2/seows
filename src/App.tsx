f4f45c7220dcb834865305464bfeb2c400a60264
seows2 <seows2@naver.com>
2022-05-15 17:09:48 +0900

    [feat] 마우스 인터렉션

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
+import Mouse from "./Components/Mouse";
 import { currentDay, getHour } from "./utils/date";
 
-type DayTime = "night" | "day";
+export type DayTime = "night" | "day";
 
 function App() {
   const [dayTime, setDayTime] = useState<DayTime>(currentDay(getHour()));
 
-  return <div className="App">{dayTime}</div>;
+  return (
+    <div className="App">
+      <Mouse dayTime={dayTime} />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Mouse/index.tsx
+++ src/Components/Mouse/index.tsx
@@ -1,7 +1,61 @@
-import { useState } from "react";
+import { DayTime } from "@/App";
+import { useEffect } from "react";
+import { useTrail, animated } from "react-spring";
+import styles from "./Mouse.module.scss";
 
-const Mouse = () => {
-  const [mousePos, setMousePos] = useState({ x: 0, y: 0, opacity: 0 });
+type MouseProps = {
+  dayTime: DayTime;
+  className?: string;
+};
+
+export type MousePos = { x: number; y: number; opacity: number };
+
+const fastPointer = { tension: 1200, friction: 40 };
+const slowPointer = { mass: 10, tension: 200, friction: 50 };
+const trans = (x: number, y: number) =>
+  `translate3d(${x}px,${y}px,0) translate3d(-50%,-50%,0)`;
+
+const Mouse = ({ className, dayTime }: MouseProps) => {
+  const [trail, api] = useTrail(3, (i) => ({
+    xy: [0, 0],
+    from: { opacity: 0, xy: [0, 0] },
+    config: i === 0 ? fastPointer : slowPointer,
+  }));
+
+  const handleMouseMove = (e: MouseEvent) => {
+    if (
+      e.clientX < 10 ||
+      e.clientX > window.innerWidth - 10 ||
+      e.clientY < 10 ||
+      e.clientY > window.innerHeight - 10
+    ) {
+      handleMouseOut();
+    } else {
+      api.start({ xy: [e.clientX - 5, e.clientY - 5], opacity: 1 });
+    }
+  };
+
+  const handleMouseOut = () => {
+    api.set({ opacity: 0 });
+  };
+
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <div className={styles.Mouse}>
+      {trail.map((props, idx) => (
+        <animated.div
+          key={idx}
+          style={{ transform: props.xy.to(trans), opacity: props.opacity }}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Mouse;
