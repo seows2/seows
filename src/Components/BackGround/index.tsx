3cc393344b44e883455b8aeea4f6c7ab4acfaef9
seows2 <seows2@naver.com>
2022-05-23 00:13:32 +0900

    [chore] 이미지 경로 변경

--- src/Components/BackGround/index.tsx
+++ src/Components/BackGround/index.tsx
@@ -9,7 +9,7 @@ const Clouds = Array.from({ length: 3 }).map((_, idx) => (
   <S.Cloud
     key={idx}
     className="cloud"
-    src="/cloud.png"
+    src="cloud.png"
     delay={idx * 10}
     width={Math.floor(Math.random() * 200) + 200}
     height={Math.floor(Math.random() * 100) + 100}
@@ -23,8 +23,8 @@ const BackGround = ({ dayTime }: BackGroundProps) => {
     <S.BackGroundWrapper>
       <S.BackGround dayTime={dayTime}>
         <div className="sky">
-          <img className="sun" src="/sun.png" alt="sun" />
-          <img className="moon" src="/moon.png" alt="moon" />
+          <img className="sun" src="sun.png" alt="sun" />
+          <img className="moon" src="moon.png" alt="moon" />
           {Clouds}
         </div>
       </S.BackGround>
